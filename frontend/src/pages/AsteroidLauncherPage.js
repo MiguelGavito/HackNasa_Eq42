f44253b99da0b718244218ba8131d19af6a4ab1e
Cruz <A01738823@tec.mx>
2025-10-04 16:30:02 -0600

    JS change

--- frontend/src/pages/AsteroidLauncherPage.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import AsteroidLauncher from '../components/AsteroidLauncher';
-
-const AsteroidLauncherPage = () => {
-  return <AsteroidLauncher />;
-};
-
-export default AsteroidLauncherPage;
